b8dcccf24a6ae7ab5acbc15d55eca214f17f3161
Rotthin <rotthin_dev@protonmail.com>
2022-06-17 04:25:15 +0200

    Removed reference object

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -10,7 +10,6 @@ m_Player(new Player()),
 m_PauseRect(new od::UI::ColorRect({200, 200, 200, 128}, {0,0}, {500,700}, ANCHORS_CENTER)),
 od::Scene({255,255,255,255}) {
 	m_PauseRect->m_Visible = false;
-
 	od::Font *font = od::Game::GetInstance()->GetFont();
 	m_PauseRect->AddChildElement(std::shared_ptr<od::UI::Button>(new od::UI::Button(font, "Exit to menu", std::bind(&GameScene::ExitToMenu, this), {0, 100}, {150, 60}, {od::UI::Anchor::Center, od::UI::Anchor::End})));
 	m_PauseRect->AddChildElement(std::shared_ptr<od::UI::Button>(new od::UI::Button(font, "Exit to desktop", std::bind(&od::Game::ShutdownWithoutReason, od::Game::GetInstance()), {0, 20}, {150, 60}, {od::UI::Anchor::Center, od::UI::Anchor::End})));
@@ -58,10 +57,6 @@ void GameScene::Tick() {
 }
 
 void GameScene::Render2D() {
-	// Reference object
-	od::Renderer::RenderQuad({0,0}, {50,50}, {255, 0, 255, 255});
-	od::Renderer::RenderText("Reference Object", Oredustry::GetInstance()->GetFont(), {0,0}, {50, 200, 164, 255}, 1.0f);
-
 	for(Ore &ore : m_Ores)
 		ore.Render();
 
