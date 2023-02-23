d557103b3b4ce82c9b1ca72fedae913c8abb826f
rxn7 <rotthin_dev@protonmail.com>
2023-02-23 18:37:04 +0100

    Fixed run_wsl.sh

--- run_wsl.sh
+++ run_wsl.sh
@@ -1,5 +1,7 @@
 # NOTE: This is only for WSL
 
 #!/usr/bin/env bash
-output=$(powershell.exe ./run.ps1)
+pushd bin/windows > /dev/null 2>&1
+output=$(./oredustry.exe)
+popd > /dev/null 2>&1
 echo $output
